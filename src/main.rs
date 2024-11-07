9575cb7998bbe76ba7edf3aba60cc185925e6f1b
sarukiti <sarukiti1891@gmail.com>
2024-11-07 21:11:31 +0900

    smdのANGLE追加

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use safe_drive:: {
     context::Context, error::DynError, logger::Logger, pr_error, topic::subscriber::Subscriber,
 };
-use drobo_interfaces::msg::{MdLibMsg, SdLibMsg, BlMdLibMsg};
+use drobo_interfaces::msg::{MdLibMsg, SmdLibMsg, BlMdLibMsg};
 use rusb::{DeviceHandle, GlobalContext};
 use motor_lib;
 
 use std::sync::LazyLock;
 
-static HANDLE: LazyLock<DeviceHandle<GlobalContext>> = LazyLock::new(|| motor_lib::init_usb_handle(0x483, 0x5740, 1));
+static HANDLE: LazyLock<DeviceHandle<GlobalContext>> = LazyLock::new(|| motor_lib::init_usb_handle(0x483, 0x5740, 1).unwrap());
 
 #[async_std::main]
 async fn main() -> Result<(), DynError> {
     let ctx = Context::new()?;
     let node = ctx.create_node("d_motor_ros", None, Default::default())?;
     let md_subscriber = node.create_subscriber::<MdLibMsg>("md_driver_topic", None)?;
+    let smd_subscriber = node.create_subscriber::<SmdLibMsg>("smd_driver_topic", None)?;
     let blmd_subscriber = node.create_subscriber::<BlMdLibMsg>("blmd_driver_topic", None)?;
     let md_task = async_std::task::spawn(md_receiver(md_subscriber, &HANDLE));
+    let smd_task = async_std::task::spawn(smd_receiver(smd_subscriber, &HANDLE));
     let blmd_task = async_std::task::spawn(blmd_receiver(blmd_subscriber, &HANDLE));
     
     md_task.await?;
@@ -33,7 +35,7 @@ async fn md_receiver(mut subscriber: Subscriber<MdLibMsg>, handle_: &DeviceHandl
                 motor_lib::md::Mode::SPEED => motor_lib::md::send_speed(handle_, msg.address, msg.power as i16),
                 motor_lib::md::Mode::ANGLE => motor_lib::md::send_angle(handle_, msg.address, msg.angle as i16),
                 _ => motor_lib::md::receive_status(handle_, msg.address)
-            } {
+            } {  
                 Ok(t) => break t,
                 Err(e) => {
                     pr_error!(logger, "{:?}", e);
@@ -42,6 +44,25 @@ async fn md_receiver(mut subscriber: Subscriber<MdLibMsg>, handle_: &DeviceHandl
         };
     }
 }
+
+async fn smd_receiver(mut subscriber: Subscriber<SmdLibMsg>, handle_: &DeviceHandle<GlobalContext>) -> Result<(), DynError> {
+    let logger = Logger::new("smd_receiver");
+    loop{
+        let msg = subscriber.recv().await?;
+        let status = loop {
+            match match msg.mode {
+                motor_lib::smd::Mode::ANGLE => motor_lib::smd::send_angle(handle_, msg.address, msg.port,msg.angle),
+                _ => motor_lib::smd::receive_status(handle_, msg.address)
+            } {
+                Ok(t) => break t,
+                Err(e) => {
+                    pr_error!(logger, "{:?}", e);
+                }
+            };
+        };
+    }
+}
+
 async fn blmd_receiver(mut subscriber: Subscriber<BlMdLibMsg>, handle_: &DeviceHandle<GlobalContext>) -> Result<(), DynError> {
     let logger = Logger::new("blmd_receiver");
     loop{
